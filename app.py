613c12b695a614dfd04289fbdca3f5c1aac97ff0
Pooja123-thakur <pooja.thakur.9484941@gmail.com>
2025-07-26 16:58:04 +0530

    push the changes

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, jsonify, request
 import requests
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app = Flask(__name__)
 API_KEY = 'CR217__olohp7Kc0qabokhzhK8pnqr7S'
